afb20c888bd2c27f4a93d10ad0f261215327c737
ReShweiCi <49321819+ReShweiCi@users.noreply.github.com>
2023-09-27 20:25:31 +0800

    笔记

---
+++ Code/TCPWarper.hpp
@@ -0,0 +1,140 @@
+﻿#pragma once
+/*
+* A SDL_Net based TCP warper to solve sticky packet problem
+* 2023.09.27 by ReShweiCi
+*/
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include <string>
+#include <spdlog/spdlog.h>
+#include <vector>
+#define BUFFER_SIZE 1024
+#define HEADER_LENGTH sizeof(unsigned int)
+
+class TCPWarper
+{
+public:
+	TCPsocket m_ServerSocket;
+	TCPWarper(Uint16 port)
+	{
+		IPaddress ip;
+		if (SDLNet_ResolveHost(&ip, NULL, port) == -1)
+			spdlog::error("Failed to resolve host, {}\n", SDLNet_GetError());
+		m_ServerSocket = SDLNet_TCP_Open(&ip);
+		if (m_ServerSocket)
+			spdlog::info("Success to create a server on port {}\n", port);
+		else
+			spdlog::error("Failed to start server, {}\n", SDLNet_GetError());
+	}
+	TCPWarper() = default;
+	~TCPWarper()
+	{
+		SDLNet_Quit();
+	}
+	/*to solve TCP sticky packet problem*/
+	// construct the packet and send
+	int Send(TCPsocket& sock, const char* raw_data, unsigned int lenth)
+	{
+		char* package = (char*)malloc(lenth + sizeof(lenth));
+		unsigned int* len = (unsigned int*)package;
+		*len = lenth;
+		memcpy(package + HEADER_LENGTH, raw_data, lenth);
+		int data_size = SDLNet_TCP_Send(sock, package, lenth + sizeof(lenth));
+		if (data_size == -1)
+		{
+			spdlog::error("Failed to send socket data, {}", SDLNet_GetError());
+			free(package);
+			return -1;
+		}
+		else
+		{
+			free(package);
+			return data_size;
+		}
+	}
+
+	// recieve the packet and push the string into pool
+	void Recieve(TCPsocket& sock)
+	{
+		// current position stands for the byte which is going to be processed
+		int currentPos = 0;
+		unsigned int* len = (unsigned int*)malloc(HEADER_LENGTH);
+		int data_size = SDLNet_TCP_Recv(sock, buffer, BUFFER_SIZE);
+		// if there is an error
+		if (data_size == -1)
+		{
+			spdlog::error("Failed to recieve socket data, {}\n", SDLNet_GetError());
+			free(len);
+			return;
+		}
+		// read the header to load length
+		memcpy(len, buffer, HEADER_LENGTH);
+		// process all the full packet until remaining part is incomplete
+		while (currentPos + HEADER_LENGTH + *len <= BUFFER_SIZE && currentPos != data_size)
+		{
+			currentPos += HEADER_LENGTH + *len;
+			UnpackCompletePacket(buffer, *len);
+		}
+		// if all the packet have been processed
+		if (currentPos == data_size)
+		{
+			free(len);
+			return;
+		}
+		// if the last packet was cut in the body
+		if (BUFFER_SIZE - currentPos > HEADER_LENGTH)
+		{
+			//read the length
+			unsigned int* len = (unsigned int*)malloc(HEADER_LENGTH);
+			memcpy(len, buffer + currentPos, HEADER_LENGTH);
+			// allocate temp buffer to store the full packet
+			char* tempBuffer = (char*)malloc(HEADER_LENGTH + *len);
+			memcpy(tempBuffer, buffer + currentPos, BUFFER_SIZE - currentPos);
+			data_size = SDLNet_TCP_Recv(sock, tempBuffer + BUFFER_SIZE - currentPos, *len + HEADER_LENGTH - (BUFFER_SIZE - currentPos));
+			UnpackCompletePacket(tempBuffer, *len);
+			free(tempBuffer);
+			free(len);
+		}
+		// if the last packet was cut in the header
+		else
+		{
+			// copy part of header to the buffer beginning
+			memcpy(buffer, buffer + currentPos, BUFFER_SIZE - currentPos);
+			// recieve the remaining part of header
+			data_size = SDLNet_TCP_Recv(sock, buffer + BUFFER_SIZE - currentPos, HEADER_LENGTH - (BUFFER_SIZE - currentPos));
+			unsigned int* len = (unsigned int*)malloc(HEADER_LENGTH);
+			memcpy(len, buffer, HEADER_LENGTH);
+			char* tempBuffer = (char*)malloc(HEADER_LENGTH + *len);
+			// copy the header to the temp buffer
+			memcpy(tempBuffer, len, HEADER_LENGTH);
+			// recieve the body to the temp buffer
+			data_size = SDLNet_TCP_Recv(sock, tempBuffer + HEADER_LENGTH, *len);
+			UnpackCompletePacket(tempBuffer, *len);
+			free(tempBuffer);
+			free(len);
+		}
+	}
+	
+	// try to get data from the data pool
+	bool GetData(std::string* data)
+	{
+		if (m_rawDataList.empty())
+			return false;
+		char* str = m_rawDataList.front();
+		*data = str;
+		free(str);
+		m_rawDataList.erase(m_rawDataList.begin());
+		return true;
+	}
+	
+private:
+	char buffer[BUFFER_SIZE];
+	// remember to call free();
+	std::vector<char*> m_rawDataList = {};
+	void UnpackCompletePacket(char* source, unsigned int &len)
+	{
+		char* str = (char*)malloc(len);
+		memcpy(str, source + HEADER_LENGTH, len);
+		m_rawDataList.push_back(str);
+	}
+};
--- 如何解决TCP流式传输的粘包问题.md
+++ 如何解决TCP流式传输的粘包问题.md
@@ -1,7 +1,7 @@
-# 如何解决TCP流式传输的粘包问题 2023.09.24
+# 如何解决TCP流式传输的粘包问题 2023.09.24-09.27
 ## 什么导致了粘包问题
 
-首先需明确的是，<b>粘包问题不是传输层TCP协议的问题</b>，其根本原因在于TCP是流式传输的，而我们的数据往往是以不定长的数据包的形式发送的，于是多个数据包就可能同时传输到对方，如果不加以解决的话对方会无法解析传输过来的数据。所以，需要在<b>应用层</b>使用相关协议，解决粘包问题。
+首先需明确的是，<b>粘包问题不是传输层TCP协议的问题</b>，其根本原因在于TCP是**流式传输**的，而我们的数据往往是以**不定长的数据包**的形式发送的，于是多个数据包就可能同时传输到对方，如果不加以解决的话对方会无法解析传输过来的数据。所以，需要在<b>应用层</b>使用相关协议，解决粘包问题。
 
 ## 解决方案
 1. 使用现成的应用层协议（如HTTP）
@@ -9,9 +9,11 @@
 3. <b>在包头加上定长的头部来表示当前数据包的数据段长度</b>，（如4个字节的包头最大可以表示长度为2^8 - 1个字节的数据段）
 
 ![](Pictures/image-20210511191145968.png)
-## 发送和接收实践
+## 如何发送和接收
 
 ### 发送
+
+发送数据时只需要在数据前加上包头即可，在实践中仅进行了Windows平台上的TCP通讯，按理来说如果跨平台需要考虑到数据类型的**大小端**问题
 
 ### 接收
 
@@ -26,32 +28,36 @@
 3. `解包在header被切断的包();`
    1. 将部分header复制到缓存的开头
    2. 调用`Recv()`接收头部剩下的数据（注意事项同上）
-   3. 读取头部，防止包体被切断，采用`解包在body被切断的包();`的方法
+   3. 读取头部，创建临时缓存（防止这个包过大）
+   4. 将完整头部复制到临时缓存（具体看解包实现是否需要头部）
+   5. 调用`Recv()`接受body到临时缓存（注意不要覆盖头部）
+   6. `解包完整的包();`
 
 伪代码实现如下，这里使用了一个currentPos变量来记住当前读取到的位置，**注意操作时一定要记得及时更新`currentPos`**
 
 ```C++
-#define header_length sizeof(unsigned int)
+#define HEADER_LENGTH sizeof(unsigned int)
 #define BUFFER_SIZE 1024
 // BUFFER_SIZE是一次性能接收的最多数据
 data_size = TCP_Recv(sock, buffer, BUFFER_SIZE);
+// currentPos代表下一个要处理的字节
 currentPos = 0;
 /* 如果包是完整的 */
-while (currentPos + header_length + length < min(BUFFER_SIZE, data_size))
+while (currentPos + HEADER_LENGTH + length <= BUFFER_SIZE && currentPos != data_size) 
 {
     ...// 解包完整的包
-    currentPos += (header_length + length);
+    currentPos += (HEADER_LENGTH + length);
 }
 /* 如果最后一个包在body被切断 */
-if (BUFFER_SIZE - currentPos > header_length)
+if (BUFFER_SIZE - currentPos > HEADER_LENGTH)
 {
     // 读取包头，分配临时的缓存(防止包的长度超过了BUFFER_SIZE)
     len = *header;// 读取包头
-    temp_buffer = malloc(len + header_length);//记得调用free();
+    temp_buffer = malloc(len + HEADER_LENGTH);//记得调用free();
     // 把buffer中剩余的部分copy到temp_buffer
-    memcpy(body, buffer + currentPos, BUFFER_SIZE - currentPos);
+    memcpy(temp_buffer, buffer + currentPos, BUFFER_SIZE - currentPos);
     // 接受包被切掉的部分
-    TCP_Recv(sock, temp_buffer + len - (BUFFER_SIZE - currentPos));
+    TCP_Recv(sock, temp_buffer + (BUFFER_SIZE - currentPos), len + HEADER_LENGTH - (BUFFER_SIZE - currentPos));
     ...// 解包完整的包
 }
 /* 如果最后一个包在header被切断 */
@@ -61,17 +67,25 @@ else
     // 把buffer剩余的部分拷贝到buffer的开头
     memcpy(buffer, buffer + currentPos, BUFFER_SIZE - currentPos);
     // 接受包剩下的头部
-    TCP_Recv(sock, buffer, header_length - (BUFFER_SIZE - currentPos));
-    // 当前位置一定要记得改动
-    currentPos = header_length;
+    TCP_Recv(sock, buffer + BUFFER_SIZE - currentPos, HEADER_LENGTH - (BUFFER_SIZE - currentPos));
     // 读取包头，分配临时的缓存(防止包的长度超过了BUFFER_SIZE)
     len = *header;// 读取包头
-    temp_buffer = malloc(len + header_length);//记得调用free();
-    // 把buffer中剩余的部分copy到temp_buffer
-    memcpy(body, buffer + currentPos, BUFFER_SIZE - currentPos);
-    // 接受包被切掉的部分
-    TCP_Recv(sock, temp_buffer + len - (BUFFER_SIZE - currentPos));
+    temp_buffer = malloc(len + HEADER_LENGTH);//记得调用free();
+    // 复制包头到临时缓存
+    memcpy(temp_buffer, len, HEADER_LENGTH);
+    // 接受包体到临时缓存
+    TCP_Recv(sock, temp_buffer + HEADER_LENGTH, len);
     ...// 解包完整的包
 }
 
 ```
+
+## 实践
+
+采用SDL_Net作为基础网络库进行了实现，这个库对Windows平台的TCP、UDP进行了简单的封装
+
+[TCPWarper.hpp](./Code/TCPWarper.hpp)
+
+## 参考
+* [TCP数据粘包的处理](https://subingwen.cn/linux/tcp-data-package/)
+* [【TCP/IP】Nagle 算法以及所谓 TCP 粘包](https://www.cnblogs.com/jojop/p/14376423.html)
